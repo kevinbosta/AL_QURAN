3380290c90527414b83876af9ca5deb81a058e7e
jamal1408 <12150113804@students.uin-suska.ac.id>
2023-06-14 21:23:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/al_quran/Models/SurahModel/Chapters.java
@@ -0,0 +1,27 @@
+package com.example.al_quran.Models.SurahModel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Chapters {
+
+	@SerializedName("chapters")
+	private List<ChaptersItem> chapters;
+
+	public void setChapters(List<ChaptersItem> chapters){
+		this.chapters = chapters;
+	}
+
+	public List<ChaptersItem> getChapters(){
+		return chapters;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Chapters{" +
+			"chapters = '" + chapters + '\'' + 
+			"}";
+		}
+}
